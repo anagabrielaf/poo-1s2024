7162d73e9b1434f3ba89c7ac8e0cdb5dcbad81cd
anagabrielaf <gabrielafrancod.s@gmailcom>
2024-03-16 11:18:44 -0300

    aula2

---
+++ aula2/main.java
@@ -0,0 +1,16 @@
+package aula2; 
+
+public class main {
+    
+    public static void main (String[] args) {
+        Calculadora  calc =  new Calculadora ();
+        calc. numA = 2.0;
+        calc. numB = 3.0;
+        System.out.println("Soma:  " + calc.somar());
+        System.out.println("Soma:  " + calc.subtrair());
+        System.out.println("Soma:  " + calc.dividir());
+        System.out.println("Soma:  " + calc.multiplicar());
+    }
+   
+
+}
