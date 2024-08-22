66a925c9eb62f1b2f8dcfc0b755819b878b4d644
anagabrielaf <gabrielafrancod.s@gmail.com>
2024-08-22 17:13:44 -0300

    adiciona

---
+++ atividade1'/variaveisOperadores.js
@@ -0,0 +1,23 @@
+
+let num1 = 7;
+let num2 = 1;
+
+
+let soma = num1 + num2;
+console.log(`Resultado da soma entre ${num1} e ${num2} é ${soma}`);
+
+
+let subtracao = num1 - num2;
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${subtracao}`);
+
+
+let multiplicacao = num1 * num2;
+console.log(`Resultado da multiplicação entre ${num1} e ${num2} é ${multiplicacao}`);
+
+
+let divisao = num1 / num2;
+console.log(`Resultado da divisão entre ${num1} e ${num2} é ${divisao}`);
+
+
+let modulo = num1 % num2;
+console.log(`Resultado do módulo entre ${num1} e ${num2} é ${modulo}`);
